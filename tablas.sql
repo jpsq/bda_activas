ba5ae52ccddcb41799b775be7ec607e166250d4f
Juan Pablo Saluzzo <saluzzojuampi@gmail.com>
2023-03-31 17:13:13 -0300

    second commit

---
+++ tablas.sql
@@ -0,0 +1,9 @@
+CREATE Table
+    socio(
+        socio_id int not null,
+        socio_nombre VARCHAR(40) not null,
+        socio_apellido VARCHAR(40) not null,
+        socio_fecha_nacimiento date,
+        socio_sexo char,
+        socio_
+    )
